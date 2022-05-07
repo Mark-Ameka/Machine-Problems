d3637dc61d07fa1fa2a2356f17414d46c98eeebe
Mark-Ameka <76719987+Mark-Ameka@users.noreply.github.com>
2022-05-07 22:14:12 +0800

    Update Machine_Problem_5_-_ChocoList_Remastered_Mendoza.c

--- Source Code/Machine_Problem_5_-_ChocoList_Remastered_Mendoza.c
+++ Source Code/Machine_Problem_5_-_ChocoList_Remastered_Mendoza.c
@@ -1,3 +1,8 @@
+/***************************************/
+/* Accomplished by: Dhony Mark Mendoza */
+/* Date: March 7, 2022                 */
+/***************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
